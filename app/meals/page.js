e11fc4fe61744f926c3e7fec5a6bc11c534d04e1
David Lorenz <dudi.lorenz@gmail.com>
2025-09-05 14:52:36 +0300

    add DB

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,26 @@
+import Link from 'next/link';
+import MealsGrid from '@/components/meals/meal-grid';
+import styles from './page.module.css';
+
 
 export default function MealsPage() {
   return (
-    <main>
-        <h1>Meals Page</h1>
-    </main>
+    <>
+      <header className={styles.header}>
+        <h1>Delicious meals, created{' '}
+          <span className={styles.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipe and cook it yourself. It is easy and fun.</p>
+        <p className={`cta @{styles.cta}`}>
+          <Link href="/meals/share">Share you favorite recipe</Link>
+        </p>
+      </header>className
+      <main className={styles.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   )
 }
+
+
+
---
+++ components/meals/meal-grid.js
@@ -0,0 +1,13 @@
+import MealItem from './meal-item';
+
+import styles from './meal-grid.module.css';
+
+export default function MealGrid({meals}){
+    return
+        <ul className={styles.meals}>
+            {meals.map(meal => 
+            <li key={meal.id}>
+                <MealItem {...meal} />    
+            </li>)}
+        </ul>
+}
